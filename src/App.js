fa51345fd7af06d14184bcf5342d760e69f1539b
Ethan5899 <ethanmpowers@gmail.com>
2020-02-22 14:45:29 -0800

    fixed it?

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form name="enterZip" onSubmit={handleSubmit}>
+        <Form.Group controlId="enterZip">
+          <Form.Label>Check The Water Quality Near You</Form.Label>
+          <Form.Control type="zip" placeholder="Enter Zip Code" />
+        </Form.Group>
+        <Button type="submit" variant="primary" size="lg">
+          GO
+        </Button>
+      </Form>
     </div>
   );
 }
+
+function handleSubmit(event)
+{
+  event.preventDefault();
+  console.log("hey")
+} 
+
+
 
 export default App;
